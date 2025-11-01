05b53c650514b885df6f50bee152f589d9d7e184
Purushotham Prajapati <Purushothamprajapati7473@gmail.com>
2025-11-01 18:06:18 +0530

    makable ai

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -14,6 +14,7 @@ export default defineConfig({
         target: 'http://localhost:3001',
         changeOrigin: true,
         secure: false,
+        port:'f0e50f377925.ngrok-free.app',
       }
     }
   }
